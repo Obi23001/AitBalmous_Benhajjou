f5b5c23a46faccbdb64a043023f995b5353bbd63
Obi <abdelali.aitbalmous2020@gmail.com>
2023-12-11 21:23:33 +0100

    programme

--- Shape/src/Cercle.java
+++
@@ -1,7 +0,0 @@
-
-public class Cercle extends Shape {
-	/**
-	 * Declaration des attributs
-	 */
-	private double rayon;
-}
--- Shape/src/Shape.java
+++
@@ -1,8 +0,0 @@
-
-public class Shape {
-	/**
-	 * Declaration des attributs
-	 */
-	private String couleur;
-	
-}
